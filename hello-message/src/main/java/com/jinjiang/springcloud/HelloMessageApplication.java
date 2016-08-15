3b06e50f01fa64ec17ebfc6b649e4f5fe434f15e
TimGong <timgong@gmail.com>
2016-08-15 14:21:56 +0800

    Hello-message use RabbitMQ

--- hello-message/src/main/java/com/jinjiang/springcloud/HelloMessageApplication.java
+++ hello-message/src/main/java/com/jinjiang/springcloud/HelloMessageApplication.java
@@ -1,16 +1,32 @@
 package com.jinjiang.springcloud;
 
+import com.jinjiang.HelloMsg;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @Controller
 public class HelloMessageApplication {
 
+    @Autowired
+    MsgSource msgSource;
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloMessageApplication.class, args);
 	}
+
+    @ResponseBody
+    @RequestMapping(value = "/msg/{msg}")
+    public String message(@PathVariable(value = "msg") String msg) {
+        HelloMsg helloMsg = new HelloMsg("Tim", msg);
+        msgSource.sendMessage(helloMsg);
+        return "Msg sent";
+    }
 }
