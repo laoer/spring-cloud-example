91ec6fc1b355cd70957788508d1e331b9a3fbecd
TimGong <timgong@gmail.com>
2016-08-16 10:03:08 +0800

    Add Zuul Server

--- eureka-client/src/main/java/com/jinjiang/springcloud/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/jinjiang/springcloud/EurekaClientApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
-@EnableZuulProxy
+//@EnableZuulProxy
 @EnableCircuitBreaker
 public class EurekaClientApplication {
 
