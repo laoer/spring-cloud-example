aa938828319887d8ee52cc59f10ac78deb8838a4
TimGong <timgong@gmail.com>
2016-08-22 17:24:33 +0800

    Freemaker

--- hello/src/main/java/com/jinjiang/HelloController.java
+++ hello/src/main/java/com/jinjiang/HelloController.java
@@ -5,7 +5,10 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * Created by Laoer on 16/8/19.
@@ -40,6 +43,14 @@ public class HelloController {
         String password = BCrypt.hashpw("password", BCrypt.gensalt());
         System.out.println(password);
         return String.valueOf(result);
+    }
+
+
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    public String welcome(Map<String, Object> model) {
+        model.put("name", "Laoer");
+        model.put("msg", "你好!");
+        return "welcome";
     }
 
 
---
+++ hello/src/main/resources/templates/welcome.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8" />
+    <title>Welcome</title>
+    <meta name="generator" content="BBEdit 11.6" />
+</head>
+<body>
+Welcome ${name} , ${msg}
+</body>
+</html>
